82c0dd7aa4213dbe55369286dbf34fab6dd5c7f9
Solomon yusuf <olafire03@gmail.com>
2025-03-04 19:33:06 +0100

    worked on dashboard 1

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory  } from 'vue-router';
 import Dashboard1 from './components/Dashboard1.vue';
+import Dashboard2 from './components/Dashboard2.vue';
 
 const router = createRouter({
     history: createWebHistory(),  
     routes: [
       { path: '/', name: 'Dashboard1', component: Dashboard1 },
+      { path: '/2', name: 'Dashboard2', component: Dashboard2 },
     ],
   });
   
